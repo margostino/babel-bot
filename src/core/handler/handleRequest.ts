f5436d8fcc736fd04c6481b4a30038074beb8293
Martín D'Agostino <martin.dagostino@klarna.com>
2024-07-02 11:43:19 +0200

    wip

--- src/core/handler/handleRequest.ts
+++ src/core/handler/handleRequest.ts
@@ -5,7 +5,6 @@ import { newBot } from '../../telegram'
 export const handleRequest = async (req: VercelRequest, res: VercelResponse) => {
   if (req.method === 'POST') {
     const bot = newBot()
-    await bot.telegram.setWebhook(`https://babel-bot.vercel.app/api/index`)
     await bot.handleUpdate(req.body as unknown as Update, res)
   } else {
     res.status(200).json('listening to bot events...')
--- src/telegram/newBot.ts
+++ src/telegram/newBot.ts
@@ -1,4 +1,5 @@
 import { Telegraf } from 'telegraf'
+import { message } from 'telegraf/filters'
 import { TELEGRAM_BOT_TOKEN } from '../constants'
 import { about } from './about'
 
@@ -11,6 +12,13 @@ export const newBot = () => {
   bot.command('about', about())
   //bot.on('message', reply())
   bot.hears('hi', (ctx) => ctx.reply('Hey there'))
-  bot.launch()
+  bot.on(message('text'), async (ctx) => {
+    // Explicit usage
+    await ctx.telegram.sendMessage(ctx.message.chat.id, `Hello ${ctx.state.role}`)
+
+    // Using context shortcut
+    await ctx.reply(`Hello ${ctx.state.role}`)
+  })
+  bot.launch().catch((e) => console.error(`Bot failed when launching: ${e.message}`))
   return bot
 }
