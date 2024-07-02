7518ea0e9f1d886a4c502a2520719dc9ddda7462
Martín D'Agostino <martin.dagostino@klarna.com>
2024-07-02 23:08:07 +0200

    wip

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,16 @@
 import { VercelRequest, VercelResponse } from '@vercel/node'
+import winston from 'winston'
 import { handleRequest, shouldHandleRequest } from '../src'
+
+const logger = winston.createLogger()
 
 const handleBot = async (req: VercelRequest, res: VercelResponse) => {
   try {
-    console.log(JSON.stringify(req))
+    console.log(JSON.stringify(req.body))
+    console.log(JSON.stringify(req.headers))
+    logger.info(JSON.stringify(req.body))
+    logger.info(JSON.stringify(req.headers))
+
     if (!shouldHandleRequest(req)) {
       res.status(401).json({
         error: 'Unauthorized',
