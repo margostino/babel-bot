5f95dceb27bcefde0bdff71ec9c390ba0e6191a8
Martín D'Agostino <martin.dagostino@klarna.com>
2024-07-03 21:09:53 +0200

    wip

--- src/telegram/newBot.ts
+++ src/telegram/newBot.ts
@@ -1,6 +1,6 @@
 import { Telegraf } from 'telegraf'
 import { message } from 'telegraf/filters'
-import { TELEGRAM_BOT_TOKEN } from '../constants'
+import { TELEGRAM_BOT_TOKEN, TELEGRAM_BOT_WEBHOOK_URL } from '../constants'
 import { reply } from './reply'
 
 const newBot = async () => {
@@ -8,22 +8,22 @@ const newBot = async () => {
     throw new Error('BOT_TOKEN must be provided!')
   }
   const bot = new Telegraf(TELEGRAM_BOT_TOKEN)
-
-  // const getWebhookInfo = await bot.telegram.getWebhookInfo();
-  // if (getWebhookInfo.url !== TELEGRAM_BOT_WEBHOOK_URL + '/api') {
-  //   console.log(`deleting webhook ${TELEGRAM_BOT_WEBHOOK_URL}`);
-  //   await bot.telegram.deleteWebhook();
-  //   console.log(`setting webhook: ${TELEGRAM_BOT_WEBHOOK_URL}/api/index`);
-  //   await bot.telegram.setWebhook(`${TELEGRAM_BOT_WEBHOOK_URL}/api/index`);
-  // }
-
   bot.start((ctx) => ctx.reply('Welcome!'))
   bot.on(message('text'), async (ctx) => {
     const babelResponse = await reply(ctx.message.text)
     ctx.telegram.sendChatAction(ctx.message.chat.id, 'typing')
     await ctx.telegram.sendMessage(ctx.message.chat.id, babelResponse)
   })
-  //bot.launch().catch((e) => console.error(`bot failed when launching: ${e.message}`))
+  bot
+    .launch({
+      allowedUpdates: ['message'],
+      webhook: {
+        domain: TELEGRAM_BOT_WEBHOOK_URL,
+        path: '/api/index',
+        port: 443,
+      },
+    })
+    .catch((e) => console.error(`bot failed when launching: ${e.message}`))
   return bot
 }
 
