97f9b0dcebf404a883745de1d16518b6525fd18e
Martín D'Agostino <martin.dagostino@klarna.com>
2024-07-03 21:04:38 +0200

    wip

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,11 @@
 // TODO: validate
 
+export const HEADERS = {
+  AUTHORIZATION: 'authorization',
+  X_BABEL_ADMIN_TOKEN: 'x-babel-admin-token',
+  X_TELEGRAM_BOT_API_SECRET_TOKEN: 'x-telegram-bot-api-secret-token',
+}
+
 export const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN || ''
 
 export const TELEGRAM_BOT_API_SECRET_TOKEN = process.env.TELEGRAM_BOT_API_SECRET_TOKEN || ''
@@ -8,8 +14,4 @@ export const BABEL_API_TOKEN = process.env.BABEL_API_TOKEN || ''
 
 export const BABEL_API_HOST = process.env.BABEL_API_HOST || ''
 
-export const HEADERS = {
-  AUTHORIZATION: 'authorization',
-  X_BABEL_ADMIN_TOKEN: 'x-babel-admin-token',
-  X_TELEGRAM_BOT_API_SECRET_TOKEN: 'x-telegram-bot-api-secret-token',
-}
+export const TELEGRAM_BOT_WEBHOOK_URL = process.env.TELEGRAM_BOT_WEBHOOK_URL || ''
--- src/telegram/newBot.ts
+++ src/telegram/newBot.ts
@@ -3,18 +3,27 @@ import { message } from 'telegraf/filters'
 import { TELEGRAM_BOT_TOKEN } from '../constants'
 import { reply } from './reply'
 
-const newBot = () => {
+const newBot = async () => {
   if (!TELEGRAM_BOT_TOKEN) {
     throw new Error('BOT_TOKEN must be provided!')
   }
   const bot = new Telegraf(TELEGRAM_BOT_TOKEN)
+
+  // const getWebhookInfo = await bot.telegram.getWebhookInfo();
+  // if (getWebhookInfo.url !== TELEGRAM_BOT_WEBHOOK_URL + '/api') {
+  //   console.log(`deleting webhook ${TELEGRAM_BOT_WEBHOOK_URL}`);
+  //   await bot.telegram.deleteWebhook();
+  //   console.log(`setting webhook: ${TELEGRAM_BOT_WEBHOOK_URL}/api/index`);
+  //   await bot.telegram.setWebhook(`${TELEGRAM_BOT_WEBHOOK_URL}/api/index`);
+  // }
+
   bot.start((ctx) => ctx.reply('Welcome!'))
   bot.on(message('text'), async (ctx) => {
     const babelResponse = await reply(ctx.message.text)
     ctx.telegram.sendChatAction(ctx.message.chat.id, 'typing')
     await ctx.telegram.sendMessage(ctx.message.chat.id, babelResponse)
   })
-  bot.launch().catch((e) => console.error(`bot failed when launching: ${e.message}`))
+  //bot.launch().catch((e) => console.error(`bot failed when launching: ${e.message}`))
   return bot
 }
 
