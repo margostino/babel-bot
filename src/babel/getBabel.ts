6c27abe4777747725856eb260d97b40c2cb87b06
Martín D'Agostino <martin.dagostino@klarna.com>
2024-07-03 15:18:23 +0200

    wip

--- src/babel/getBabel.ts
+++ src/babel/getBabel.ts
@@ -13,13 +13,17 @@ export const getBabel = async (query: string): Promise<any> => {
     })
 
     if (response.status === 200 && response.data && response.data.message) {
+      console.log('got successful response from Babel API')
       return response.data.message.response
     } else if (response.status === 200) {
+      console.log('got unexpected response from Babel API')
       throw new Error(`Error: Unexpected response ${response.data}`)
     } else {
+      console.log('got non-success from Babel API')
       throw new Error(`Error: Received status code ${response.status}`)
     }
   } catch (error) {
+    console.log('unexpected failure calling Babel API')
     throw new Error(`Failed calling Babel API: ${(error as Error).message}`)
   }
 }
--- src/telegram/reply.ts
+++ src/telegram/reply.ts
@@ -1,5 +1,6 @@
 import { getBabel } from '../babel'
 
 export const reply = async (query: string) => {
+  console.log('getting response from Babel API...')
   return await getBabel(query)
 }
