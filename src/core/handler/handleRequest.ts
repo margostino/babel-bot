eb862cb960542afb5d3586d57362ab800f457b7e
Martín D'Agostino <martin.dagostino@klarna.com>
2024-07-04 20:23:47 +0200

    wip

--- src/core/handler/handleRequest.ts
+++ src/core/handler/handleRequest.ts
@@ -1,10 +1,16 @@
 import { VercelRequest, VercelResponse } from '@vercel/node'
+import { logger } from '../../logger'
 import { handleMessage } from '../../telegram'
 
 export const handleRequest = async (req: VercelRequest, res: VercelResponse) => {
   if (req.method === 'POST') {
-    res.status(200).json('handling events...')
-    handleMessage(req, res)
+    try {
+      await handleMessage(req, res)
+      res.status(200).json('ok')
+    } catch (error) {
+      logger.error('Error handling message:', error)
+      res.status(500).json('Internal server error')
+    }
   } else {
     res.status(200).json('listening to bot events...')
   }
--- src/telegram/handleMessage.ts
+++ src/telegram/handleMessage.ts
@@ -32,26 +32,19 @@ export const handleMessage = async (req: VercelRequest, res: VercelResponse) => 
     const userMessage = body.message?.text
 
     if (chatId) {
-      try {
-        if (userMessage && chatId) {
-          logger.info(`Received message: ${userMessage} from chat ID: ${chatId}`)
-          const response = await reply(userMessage)
-          await sendMessage(chatId, response)
-        } else {
-          logger.info('No message found in the request')
-        }
-        //setWebHook()
-        res.status(200).json('done')
-      } catch (error) {
-        console.error('Error handling update:', error)
-        res.status(500).json('Internal server error')
+      if (userMessage && chatId) {
+        logger.info(`Received message: ${userMessage} from chat ID: ${chatId}`)
+        const response = await reply(userMessage)
+        await sendMessage(chatId, response)
+      } else {
+        logger.info('No message found in the request')
       }
+      //setWebHook()
     } else {
-      res.status(400).json('Bad Request: Chat ID not found')
+      logger.error('Bad Request: Chat ID not found')
     }
-
     //bot.handleUpdate(req.body as unknown as Update, res)
   } else {
-    res.status(200).json('listening to bot events...')
+    logger.info('not a POST request')
   }
 }
