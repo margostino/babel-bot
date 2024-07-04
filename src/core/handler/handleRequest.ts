2e4e00378c19552dab78011067f7768f380b0e89
Martín D'Agostino <martin.dagostino@klarna.com>
2024-07-04 15:24:07 +0200

    wip

--- src/core/handler/handleRequest.ts
+++ src/core/handler/handleRequest.ts
@@ -1,28 +1,26 @@
 import { VercelRequest, VercelResponse } from '@vercel/node'
-import { message } from 'telegraf/filters'
-import { TELEGRAM_BOT_API_SECRET_TOKEN, TELEGRAM_BOT_WEBHOOK_URL } from '../../constants'
-import { logger } from '../../logger'
-import { bot, reply } from '../../telegram'
+import { Update } from 'telegraf/typings/core/types/typegram'
+import { bot } from '../../telegram'
 
 export const handleRequest = async (req: VercelRequest, res: VercelResponse) => {
   if (req.method === 'POST') {
-    //bot.handleUpdate(req.body as unknown as Update, res)
-    bot.on(message('text'), async (ctx) => {
-      const babelResponse = await reply(ctx.message.text)
-      ctx.telegram.sendChatAction(ctx.message.chat.id, 'typing')
-      await ctx.telegram.sendMessage(ctx.message.chat.id, babelResponse)
-    })
-    // bot.launch().catch((e) => logger.error(`bot failed when launching: ${e.message}`))
-    bot
-      .launch({
-        allowedUpdates: ['message'],
-        webhook: {
-          domain: TELEGRAM_BOT_WEBHOOK_URL,
-          secretToken: TELEGRAM_BOT_API_SECRET_TOKEN,
-          path: '/api/index',
-        },
-      })
-      .catch((e) => logger.error(`bot failed when launching: ${e.message}`))
+    bot.handleUpdate(req.body as unknown as Update, res)
+    // bot.on(message('text'), async (ctx) => {
+    //   const babelResponse = await reply(ctx.message.text)
+    //   ctx.telegram.sendChatAction(ctx.message.chat.id, 'typing')
+    //   await ctx.telegram.sendMessage(ctx.message.chat.id, babelResponse)
+    // })
+    // // bot.launch().catch((e) => logger.error(`bot failed when launching: ${e.message}`))
+    // bot
+    //   .launch({
+    //     allowedUpdates: ['message'],
+    //     webhook: {
+    //       domain: TELEGRAM_BOT_WEBHOOK_URL,
+    //       secretToken: TELEGRAM_BOT_API_SECRET_TOKEN,
+    //       path: '/api/index',
+    //     },
+    //   })
+    //   .catch((e) => logger.error(`bot failed when launching: ${e.message}`))
   } else {
     res.status(200).json('listening to bot events...')
   }
--- src/telegram/newBot.ts
+++ src/telegram/newBot.ts
@@ -1,5 +1,8 @@
 import { Telegraf } from 'telegraf'
+import { message } from 'telegraf/filters'
 import { TELEGRAM_BOT_TOKEN } from '../constants'
+import { logger } from '../logger'
+import { reply } from './reply'
 
 const newBot = () => {
   if (!TELEGRAM_BOT_TOKEN) {
@@ -7,6 +10,23 @@ const newBot = () => {
   }
   const bot = new Telegraf(TELEGRAM_BOT_TOKEN)
   bot.start((ctx) => ctx.reply('Welcome!'))
+  bot.on(message('text'), async (ctx) => {
+    const babelResponse = await reply(ctx.message.text)
+    ctx.telegram.sendChatAction(ctx.message.chat.id, 'typing')
+    await ctx.telegram.sendMessage(ctx.message.chat.id, babelResponse)
+  })
+  bot.launch().catch((e) => logger.error(`bot failed when launching: ${e.message}`))
+  // bot
+  //   .launch({
+  //     allowedUpdates: ['message'],
+  //     webhook: {
+  //       domain: TELEGRAM_BOT_WEBHOOK_URL,
+  //       secretToken: TELEGRAM_BOT_API_SECRET_TOKEN,
+  //       path: '/api/index',
+  //     },
+  //   })
+  //   .catch((e) => logger.error(`bot failed when launching: ${e.message}`))
+
   return bot
 }
 
