fed8a731c7ac79afd7e095d1964ea4d87ea9855d
Martín D'Agostino <martin.dagostino@klarna.com>
2024-07-04 14:16:27 +0200

    wip

--- src/auth/isAuthorized.ts
+++ src/auth/isAuthorized.ts
@@ -2,8 +2,5 @@ import { VercelRequest } from '@vercel/node'
 import { HEADERS, TELEGRAM_BOT_API_SECRET_TOKEN } from '../constants'
 
 export const isAuthorized = (request: VercelRequest) => {
-  const x =
-    request.headers[HEADERS.X_TELEGRAM_BOT_API_SECRET_TOKEN] === TELEGRAM_BOT_API_SECRET_TOKEN
-  console.log('isAuthorized', x)
-  return x
+  return request.headers[HEADERS.X_TELEGRAM_BOT_API_SECRET_TOKEN] === TELEGRAM_BOT_API_SECRET_TOKEN
 }
